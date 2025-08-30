20b9f0a99d883edca91b3423333fde6814ce8d89
Zihao <zihaozhang@seu.edu.cn>
2025-08-30 13:13:25 +0800

    add export=PYTHONPATH

--- tools/uniad_vis_result.sh
+++ tools/uniad_vis_result.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+export PYTHONPATH="${PYTHONPATH}: {$pwd}"
+
+# python ./tools/analysis_tools/visualize/run.py \
+#     --predroot PATH_TO_YOUR_PREDISION_RESULT_PKL \
+#     --out_folder PATH_TO_YOUR_OUTPUT_FOLDER \
+#     --demo_video FILENAME_OF_OUTPUT_VIDEO \
+#     --project_to_cam True
 
 python ./tools/analysis_tools/visualize/run.py \
-    --predroot PATH_TO_YOUR_PREDISION_RESULT_PKL \
-    --out_folder PATH_TO_YOUR_OUTPUT_FOLDER \
-    --demo_video FILENAME_OF_OUTPUT_VIDEO \
+    --predroot /data1/zzh/UniAD-2.0/output/results.pkl \
+    --out_folder /data1/zzh/UniAD-2.0/output/ \
+    --demo_video test_demo.avi \
     --project_to_cam True
